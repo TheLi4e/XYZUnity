a3ab182fc55d9beb38e9222a11896740d87c6131
Виктор Василик <li4e99@gmail.com>
2024-03-30 17:37:13 +0200

    Legacy Input

---
+++ XYZUnity/Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        _hero.SetDirection(horizontal);
+
+        if (Input.GetButtonUp("Fire1"))
+            _hero.SaySomething();
+    }
+}
