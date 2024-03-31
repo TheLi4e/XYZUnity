0216d2f6342c6b50b3af4b65acf6ddc678f6cbf5
Виктор Василик <li4e99@gmail.com>
2024-03-31 19:07:43 +0200

    Added Tilemap and coin

---
+++ XYZUnity/Assets/Scripts/Components/DestroyObjectComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Components
+{
+    public class DestroyObjectComponent : MonoBehaviour
+    {
+        [SerializeField] GameObject _objectToDestroy;
+
+        public void DestroyObject()
+        {
+            Destroy(_objectToDestroy);
+        }
+    }
+}
