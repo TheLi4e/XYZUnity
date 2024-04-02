ecdab7b82afb481eee24dc53d9f7292e9f5249b3
Виктор Василик <li4e99@gmail.com>
2024-04-02 20:10:46 +0200

    Added animations (jump and fall), update Hero script

--- XYZUnity/Assets/Scripts/Hero.cs
+++ XYZUnity/Assets/Scripts/Hero.cs
@@ -12,10 +12,12 @@ namespace Scripts
 
         private Rigidbody2D _rigidbody;
         private Vector2 _direction;
+        private Animator _animator;
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
         }
 
         public void SetDirection(Vector2 direction)
@@ -28,14 +30,20 @@ namespace Scripts
             _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
 
             bool isJumping = _direction.y > 0;
+            var isGrounded = IsGrounded();
+
             if (isJumping)
             {
-                if (IsGrounded())
+                if (isGrounded)
                     _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
 
             }
             else if (_rigidbody.velocity.y > 0)
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+
+            _animator.SetBool("is-ground", isGrounded);
+            _animator.SetFloat("vertical-velocity", _rigidbody.velocity.y);
+            _animator.SetBool("is-running", _direction.x != 0);
         }
 
         private bool IsGrounded()
