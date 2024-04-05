00797dc20e3510a69618d15312604a019da57c62
Виктор Василик <li4e99@gmail.com>
2024-04-05 18:41:57 +0200

    Added manual particles

---
+++ XYZUnity/Assets/Scripts/Components/SpawnComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Components
+{
+    internal class SpawnComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private GameObject _prefab;
+
+        [ContextMenu("Spawn")]
+
+        public void Spawn()
+        {
+            Instantiate(_prefab, _target.position, Quaternion.identity);
+        }
+    }
+}
