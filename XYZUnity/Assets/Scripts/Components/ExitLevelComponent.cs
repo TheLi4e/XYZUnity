027a7b6cd065cce31a4397fecadc6ad0efb9b44e
Виктор Василик <li4e99@gmail.com>
2024-04-09 22:07:19 +0200

    ref

---
+++ XYZUnity/Assets/Scripts/Components/ExitLevelComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Components
+{
+    internal class ExitLevelComponent :MonoBehaviour
+    {
+        [SerializeField] private string _sceneName;
+
+        public void Exit()
+        {
+            SceneManager.LoadScene(_sceneName);
+        }
+    }
+}
