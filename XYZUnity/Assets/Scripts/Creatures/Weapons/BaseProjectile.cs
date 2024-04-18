47c7f63cb61a3314e4e1af598c6e249ae67aed78
Виктор Василик <li4e99@gmail.com>
2024-04-18 22:40:39 +0200

    Added coin's levitation

---
+++ XYZUnity/Assets/Scripts/Creatures/Weapons/BaseProjectile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Scripts.Creatures.Weapons
+{
+    public class BaseProjectile : MonoBehaviour
+    {
+        [SerializeField] protected float _speed;
+        [SerializeField] private bool _invertX;
+
+        protected Rigidbody2D Rigidbody;
+        protected int Direction;
+
+        protected virtual void Start()
+        {
+            var mod = _invertX ? -1 : 1;
+            Direction = mod * transform.lossyScale.x > 0 ? 1 : -1;
+            Rigidbody = GetComponent<Rigidbody2D>();
+        }
+    }
+}
