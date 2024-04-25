f8ab0678e6c13ca8078f0ddd2f849c9ef8780605
Виктор Василик <li4e99@gmail.com>
2024-04-25 21:49:03 +0200

    Added MainMenuWindow and SettigsWindow

---
+++ XYZUnity/Assets/UI/MainMenu/MainMenuWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.MainMenu
+{
+    public class MainMenuWindow : AnimatedWindow
+    {
+
+        private Action _closeAction;
+        public void OnShowSettings()
+        {
+            var window = Resources.Load<GameObject>("UI/SettingsWindow");
+            var canvas = FindObjectOfType<Canvas>();
+            Instantiate(window, canvas.transform);
+        }
+
+        public void OnStartGame()
+        {
+            _closeAction = () => { SceneManager.LoadScene("Level1"); };
+            Close();
+        }
+
+        public void OnExit()
+        {
+            _closeAction = () => 
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            };
+            Close();
+
+        }
+
+        public override void OnCloseAnimationComplete()
+        {
+            base.OnCloseAnimationComplete();
+            _closeAction?.Invoke();
+
+        }
+    }
+}
