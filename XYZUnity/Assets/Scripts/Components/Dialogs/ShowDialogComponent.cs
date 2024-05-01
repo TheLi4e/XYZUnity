4764355be5a1fe4d4b588afbd402728cf293f628
Виктор Василик <li4e99@gmail.com>
2024-05-01 22:43:28 +0200

    Added GreenGem and CrabbyNPC, dialog for doors

--- XYZUnity/Assets/Scripts/Components/Dialogs/ShowDialogComponent.cs
+++ XYZUnity/Assets/Scripts/Components/Dialogs/ShowDialogComponent.cs
@@ -8,23 +8,29 @@ namespace Scripts.Components.Dialogs
 {
     public class ShowDialogComponent : MonoBehaviour
     {
-        [SerializeField] private Mode _mode; 
+        [SerializeField] private Mode _mode;
         [SerializeField] private DialogData _bound;
         [SerializeField] private DialogDef _external;
 
         private DialogBoxController _dialogBox;
         public void Show()
         {
-            if (_dialogBox == null) 
+            if (_dialogBox == null)
                 _dialogBox = FindObjectOfType<DialogBoxController>();
             _dialogBox.ShowDialog(Data);
+        }
+
+        public void Show(DialogDef def)
+        {
+            _external = def;
+            Show();
         }
 
         public DialogData Data
         {
             get
             {
-                switch(_mode)
+                switch (_mode)
                 {
                     case Mode.Bound:
                         return _bound;
@@ -32,7 +38,7 @@ namespace Scripts.Components.Dialogs
                         return _external.Data;
                     default:
                         throw new ArgumentOutOfRangeException();
-                       
+
                 }
             }
         }
